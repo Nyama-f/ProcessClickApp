098205270aa94bebe2a56e2e4b0663c112783357
Nyama-f <74872487+Nyama-f@users.noreply.github.com>
2022-05-16 18:45:03 +0500

    Enabling viewBinding and implementation clicker logic. Saving data when the screen rotated using onSavedInstanceState

--- app/src/main/java/com/example/processclickapp/MainActivity.kt
+++ app/src/main/java/com/example/processclickapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.processclickapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.processclickapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var counter: Int = 0
+    private val KEY_COUNT = "COUNT"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val textView: TextView = binding.textView
+        if(savedInstanceState != null){
+            counter = savedInstanceState.getInt(KEY_COUNT, 0)
+            textView.setText("$counter")
+        }
+
+        val buttonCounter: Button =binding.buttonCounter
+        buttonCounter.setOnClickListener {
+            textView.text ="${++counter}"
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_COUNT, counter)
     }
 }
